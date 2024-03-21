6f119f2a905883a2494f8ddc23e3d1a2604323cc
nampsec <playload@qq.com>
2024-03-21 18:57:08 +0800

    fuck every people, what the fuck!

--- test3.cpp
+++ test3.cpp
@@ -5,6 +5,18 @@ auto gt(int x, float y) -> decltype(x + y)
 {
     return x + y;
 }
+struct data
+{
+    char name[40];
+    mutable int access;
+};
+
+
 void song(const char * name = "O my papa", int n = 1);
 
+int main()
+{
+    const data d = {"hello", 1};
+}
 
+// mark as the day of emo and masteratu
