e8d265676672f0bd68f2534522c04d413204c7ce
nampsec <playload@qq.com>
2024-04-11 20:04:17 +0800

    好烦

---
+++ 12_9_queue.h
@@ -0,0 +1,24 @@
+#ifndef _12_9_QUEUE_H_
+#define _12_9_QUEUE_H_
+
+class Queue
+{
+    enum {Q_SIZE = 10};te
+private:
+    /* data */
+public:
+    Queue(/* args */);
+    ~Queue();
+};
+
+Queue::Queue(/* args */)
+{
+}
+
+Queue::~Queue()
+{
+}
+
+
+
+#endif
